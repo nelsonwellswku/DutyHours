9ae0ed028e85d8b87e57e5c95ebab1ffb3bd644c
Nelson Wells <nelsonwellswku@gmail.com>
2016-05-01 17:18:41 -0500

    Add a user / shift with seed data to see analysis output in browser

--- src/DataAccess/Migrations/Configuration.cs
+++ src/DataAccess/Migrations/Configuration.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Data.Entity.Migrations;
+using Octogami.DutyHours.DataAccess.Entities;
 
 namespace Octogami.DutyHours.DataAccess.Migrations
 {
@@ -23,6 +26,46 @@ namespace Octogami.DutyHours.DataAccess.Migrations
 			//      new Person { FullName = "Rowan Miller" }
 			//    );
 			//
+
+			// Add some test users
+			var john = new ApplicationUser {UserName = "John"};
+			var patty = new ApplicationUser {UserName = "Patty"};
+			var mike = new ApplicationUser {UserName = "Mike"};
+			context.Users.AddOrUpdate(p => p.UserName,
+				john,
+				patty,
+				mike
+			);
+
+			// Add some shifts that would throw some analysis errors
+			context.Shifts.AddOrUpdate(x => x.Begin,
+			
+				// for john
+				new Shift
+				{
+					Begin = new DateTimeOffset(2000, 1, 1, 8, 0, 0, 0, TimeSpan.Zero),
+					End = new DateTimeOffset(2000, 1, 1, 17, 0, 0, 0, TimeSpan.Zero),
+					User = john
+				},
+				new Shift
+				{
+					Begin = new DateTimeOffset(2000, 1, 2, 8, 0, 0, 0, TimeSpan.Zero),
+					End = new DateTimeOffset(2000, 1, 3, 9, 0, 0, 0, TimeSpan.Zero),
+					User = john
+				},
+				new Shift
+				{
+					Begin = new DateTimeOffset(2000, 1, 4, 8, 0, 0, 0, TimeSpan.Zero),
+					End = new DateTimeOffset(2000, 1, 4, 17, 0, 0, 0, TimeSpan.Zero),
+					User = john
+				},
+				new Shift
+				{
+					Begin = new DateTimeOffset(2000, 1, 5, 8, 0, 0, 0, TimeSpan.Zero),
+					End = new DateTimeOffset(2000, 1, 5, 17, 0, 0, 0, TimeSpan.Zero),
+					User = john
+				}
+			);
 		}
 	}
 }
